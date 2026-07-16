54fa1b4a935ba58c4736e94140765e8eb3776543
R3tr0DNF <frankjumbo52@gmail.com>
2026-07-15 20:49:00 -0500

    Ajustes al distribucion sql

--- base_datos/distribucion.sql
+++ base_datos/distribucion.sql
@@ -1,32 +1,25 @@
---Distribucion de tablas con citus
-
-CREATE EXTENSION IF NOT EXISTS citus;
-
---REGISTRAR NODOS WORKER EN EL NODO COORDINADOR
-
-SELECT citus_add_node('citus-worker-1', 5432);
-SELECT citus_add_node('citus-worker-2', 5432);
-
---Verificar los workers
-
-SELECT * FROM citus_get_active_worker_nodes();
-
---Tablas de referencia (Estas se usaran para replicarse en los workers)
+-- Tablas de referencia
 
 SELECT create_reference_table('restaurantes');
+
 SELECT create_reference_table('repartidores');
 
---Distribuir las tablas de clientes y pedidos por cliente_id
+-- Tablas distribuidas
 
-SELECT create_distributed_table('clientes', 'cliente_id');
-SELECT create_distributed_table('pedidos', 'cliente_id');
+SELECT create_distributed_table('clientes','cliente_id');
 
-SELECT create_distributed_table('detalle_pedidos', 'cliente_id', colocate_with => 'pedidos');
+SELECT create_distributed_table('pedidos','cliente_id');
 
---Verificar la distribución de las tablas
+SELECT create_distributed_table(
+    'detalle_pedidos',
+    'cliente_id',
+    colocate_with=>'pedidos'
+);
 
-SELECT 
-    logicalrelid AS tabla,
-    partmethod AS metodo_particion,
-    partkey AS clave_distribucion
+-- Verificación
+
+SELECT
+    logicalrelid,
+    partmethod,
+    partkey
 FROM pg_dist_partition;
