c8e787b3555f80cd1addea2c829c3e8d22d3f939
R3tr0DNF <frankjumbo52@gmail.com>
2026-07-17 00:40:32 -0500

    Agregar los csv dentro del  coordinador y mejorar el script para evitar el error de sha

--- worker/init/02-config.sh
+++ worker/init/02-config.sh
@@ -3,10 +3,9 @@ set -e
 
 echo "Configurando pg_hba.conf..."
 
-cat >> "$PGDATA/pg_hba.conf" <<EOF
-host    all    all    172.16.0.0/12    trust
+sed -i '/^host[[:space:]]\+all[[:space:]]\+all[[:space:]]\+all[[:space:]]\+scram-sha-256/i\
+host    all    all    172.16.0.0/12    trust\
 host    all    all    192.168.0.0/24   trust
-EOF
+' "$PGDATA/pg_hba.conf"
 
 echo "Configuración aplicada."
-
